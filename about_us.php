aa1fd5044681520548cd8583b45a7aeac8b178a8
Vandana Singh <vandana1411singh@gmail.com>
2021-08-07 23:57:56 +0530

    add all files

---
+++ about_us.php
@@ -0,0 +1,73 @@
+<?php
+require "includes/common.php";
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About Us | E-Store.com</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    
+      
+<body>
+       <!-- Header -->
+    <?php include 'includes/header.php'; ?>
+    <br>
+        <div class="container">
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">WHO WE ARE</h3>
+                    <img src="./img/about-img.jpg" alt="about image"><br><br>
+                    <p>This is an advanced mobile store that adjusts according to user choice and ensures most profitability using artificial intelligence. It provides user with an easy and beautiful GUI and shows him a list of products. Users may Login/Register on the system, see products and add them to the cart. we have tried our best coding to make this website a interactive one.<p>
+                </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">HISTORY OF MOBILE PHONES</h3><br><br>
+                    <h4 style="color:blue">1957-1965&#45;</h4>
+                    <p>In the USSR, Leonoid Kupriyanovich,an enginnering form Moscow developed and preseneted a number of experimental pocket sized communications radio. The weight of one model,presented in 1961,was only 70g and could fit on a palm. However ,in the USSR the decision at first to develop the system of the automobile"Altai'phone was made. In 1965, the Bulgarain company "Radioelektranika"presented a mobile automatic phone combined with a base station at the inforga -65 internatyional wxhibition in moscow . solutions of this phone were based ona system developed by Leonid Kupriyanovich. One base station,connected to tone telephone wire line , could serve upto 15 customers.</p>
+                    <br>
+                    <h4 style="color:blue">UNDERLYING TECHNOLOGY&#45;</h4>
+                    <p>Advances in MOSFET power electronic technology also enabled the development of digital wireless mobile networks, which are essential to modern cell phones. The wide adoption of power MOSFET, LDMOS (lateral diffused MOS) and RF CMOS (radio frequency CMOS) devices led to the development and proliferation of digital wireless mobile networks by the 1990s, with further advances in MOSFET technology leading to increasing bandwidth during the 2000s.[18][19][20] Most of the essential elements of wireless mobile networks are built from MOSFETs, including the mobile transceivers, base station modules, routers, RF power amplifiers,[19] telecommunication circuits,[14] RF circuits, and radio transceivers,[20] in networks such as 2G, 3G,[18] and 4G.[19]
+
+Another important enabling factor was the lithium-ion battery, which became indispensable as an energy source for cell phones.[15] The lithium-ion battery was invented by John Goodenough, Rachid Yazami and Akira Yoshino in the 1980s,[21] and commercialized by Sony and Asahi Kasei in 1991.[22].</p>
+                    <br>
+                    </div>
+                </div>
+                </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">OPPORTUNITIES</h3><br><br>
+                        <b>Available Roles</b>
+                            <ol>
+                                <li>Jr./Sr. Web Developer [Full Time Role + also available as a 6 Months Internships]</li>
+                                <li>Bussiness Apprentise [6 Months Internships]</li>
+                                <li>Manager at backend operations[Full Time Role + also available as a 6 Months Internships]</li>
+                            </ol>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
